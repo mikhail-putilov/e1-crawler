d7b662aaa6c870b4e8a419134d7cddca3d06754b
Putilov Michael <post.snowy@gmail.com>
2013-11-26 20:24:31 +0600

    Works additional dates. Current date is not.

--- Parser.66/MainWindow.xaml.cs
+++ Parser.66/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Windows.Controls.Primitives;
 using HtmlAgilityPack;
 
 namespace Parser._66
@@ -11,7 +14,7 @@ namespace Parser._66
     public partial class MainWindow
     {
 //        const string UriString = @"http://www.e1.ru/afisha/events/gastroli";
-        const string UriString = @"http://www.e1.ru/afisha/events/gastroli/1.html";
+        private const string UriString = @"http://www.e1.ru/afisha/events/gastroli/1.html";
 
         public MainWindow()
         {
@@ -21,66 +24,75 @@ namespace Parser._66
         private void MainWindow_OnInitialized(object sender, EventArgs e)
         {
             HtmlDocument htmlPage = LoadHtmlPageFromWeb();
-            List<gigEvent> events = ExtractAllGigEvents(htmlPage);
+            List<GigEvent> events = ExtractAllGigEvents(htmlPage);
             MainDataGrid.ItemsSource = events;
         }
 
-        private List<gigEvent> ExtractAllGigEvents(HtmlDocument htmlSnippet)
+        private List<GigEvent> ExtractAllGigEvents(HtmlDocument htmlSnippet)
         {
-            var events = new List<gigEvent>();
+            var events = new List<GigEvent>();
 
             HtmlNode root = htmlSnippet.DocumentNode;
             HtmlNodeCollection pivotNodes = root.SelectNodes(@"//span[@class='small']");
             //fucked up layout
-            string date = ExtractTextAboutDate(pivotNodes);
             foreach (HtmlNode pivotNode in pivotNodes)
             {
+                string date = ExtractTextAboutDate(pivotNode);
+
                 string name = ExtractTextAboutName(pivotNode);
 
                 string placeText = ExtractTextAboutPlace(pivotNode);
 
-                events.Add(new gigEvent {Place = placeText, Date = date, Name = name});
+                events.Add(new GigEvent {Place = placeText, Date = date, Name = name});
             }
             return events;
         }
 
-        private static string ExtractTextAboutDate(HtmlNodeCollection pivotNodes)
+        private static string ExtractTextAboutDate(HtmlNode pivotNode)
         {
-            string date =
-                pivotNodes[0].SelectSingleNode(@"ancestor::table[3]//b[@class='white_menu']")
-                    .InnerText.Trim();
+            const string selector = @"ancestor::table[3]//b[@class='white_menu']";
+            const string selector2 = @"ancestor::table[1]/preceding-sibling::table//span[@class='white_menu'][last()]";
+            const string selector3 = @"(ancestor::table[1]/preceding-sibling::table//span[@class='white_menu'][last()])[last()]";
+            try
+            {
+                return ExtractText(selector3, pivotNode);
+            }
+            catch (SelectException e)
+            {
+                return null;
+            }
+        }
+
+        private static string ExtractText(string xPathSelector, HtmlNode pivotNode)
+        {
+            HtmlNode node = pivotNode.SelectSingleNode(xPathSelector);
+            if (node == null)
+                throw new SelectException(xPathSelector);
+            string date = node.InnerText.Trim();
+            if (date == string.Empty)
+                Console.Error.WriteLine("Extracted text from pivot node \"{0}\" with selector xpath \"{1}\" is empty",
+                    pivotNode.InnerText, xPathSelector);
             return date;
         }
 
         private static string ExtractTextAboutName(HtmlNode pivotNode)
         {
-            //take second table after table that is holding pivotnode:
+            //take second table after table that is holding pivotnode (special thanks to e1's front-end developers):
             //ancestor::table[1]/following-sibling::table[2]
             const string selector = @"ancestor::table[1]/following-sibling::table[2]//b[@class='big_orange']";
-            HtmlNode node = pivotNode.SelectSingleNode(selector);
-            if (node == null)
-                throw new SelectException(selector);
-
-            string name =
-                node.InnerText.Trim();
-            if (name == string.Empty)
-                Console.Error.WriteLine("name is empty for pivot node \"{0}\"", pivotNode.InnerText);
-
-            return name;
+            return ExtractText(selector, pivotNode);
         }
 
         private static string ExtractTextAboutPlace(HtmlNode pivotNode)
         {
-            string placeText = pivotNode.ParentNode.Element("b").InnerText.Trim();
-            if (placeText == string.Empty)
-                Console.Error.WriteLine("place text is empty for pivot node \"{0}\"", pivotNode.InnerText);
-            return placeText;
+            const string selector = @"../b";
+            return ExtractText(selector, pivotNode);
         }
 
         private HtmlDocument LoadHtmlPageFromWeb()
         {
-            var stream = WebRequest.Create(UriString).GetResponse().GetResponseStream();
-            if (stream == null) 
+            Stream stream = WebRequest.Create(UriString).GetResponse().GetResponseStream();
+            if (stream == null)
                 throw new EmptyResponseException(UriString);
 
             var doc = new HtmlDocument();
