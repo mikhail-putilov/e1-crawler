e425373409aee04773ac9c58275cc0b9eabf3c91
Putilov Michael <post.snowy@gmail.com>
2014-02-04 17:05:32 +0600

    Add HtmlAgilityPack, default .gitignore, exceptions and gigEvent classes.

---
+++ Parser.66/Exceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Parser._66
+{
+    internal class EmptyResponseException : Exception
+    {
+        public EmptyResponseException(string uriString) 
+            : base(string.Format("Cannot get response from \"{0}\"", uriString))
+        {
+        }
+    }
+    internal class SelectException : Exception
+    {
+        public SelectException(string path)
+            : base(string.Format("Cannot select \"{0}\"", path))
+        {
+        }
+    }
+}
---
+++ Parser.66/GigEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Parser._66
+{
+    internal class GigEvent
+    {
+        public string Date { get; set; }
+        public string Place { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
